248dfe09090cd54f7f93cbaca276f5c462592ffb
Lucia Carrera <luciaacarreraa@gmail.com>
2022-01-30 15:10:42 -0500

    finished forces

--- simulation.py
+++ simulation.py
@@ -1,6 +1,12 @@
+import pybullet_data
 import pybullet as p
 import time as t
+
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+
 p.loadSDF("box.sdf")
 for x in range(0,1001):
     t.sleep(1/60)
