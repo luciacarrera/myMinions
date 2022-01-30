8db6388e5c7ba4c5f0f06baeaffd4f33a4cd80f9
Lucia Carrera <luciaacarreraa@gmail.com>
2022-01-30 15:22:12 -0500

    finished link size & link position

--- generate.py
+++ generate.py
@@ -1,5 +1,7 @@
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length, width, height = 1, 2, 3
+x,y,z = 0,0,1.5
+pyrosim.Send_Cube(name="Box", pos=[0,0,1.5] , size=[length, width, height])
 
 pyrosim.End()
