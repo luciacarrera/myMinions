225a30f249b27178003eb7d13ef150217f68ac3c
Lucia Carrera <luciaacarreraa@gmail.com>
2022-01-31 15:21:54 -0500

    defining a robot done

--- generate.py
+++ generate.py
@@ -2,10 +2,17 @@ import pyrosim.pyrosim as pyrosim
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
+
+    # cube is part of the world
+    length, width, height = 1, 1, 1
+    x,y, z = 0,5, height/2
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height]  )
     pyrosim.End()
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
+
+    # this cube is part of the robot
     length, width, height = 1, 1, 1
     x,y, z = 0,0, height/2
     pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height]  )
@@ -14,3 +21,4 @@ def Create_Robot():
 
 Create_World()
 Create_Robot()
+print("here")
--- simulation.py
+++ simulation.py
@@ -6,6 +6,8 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+
 
 p.loadSDF("world.sdf")
 
