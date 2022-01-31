147759a0eb3557fba78af37e6728fca1ccf3904c
Lucia Carrera <luciaacarreraa@gmail.com>
2022-01-30 20:33:22 -0500

    step 15

--- generate.py
+++ generate.py
@@ -1,11 +1,16 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("world.sdf")
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length, width, height = 1, 1, 1
+    x,y, z = 0,0, height/2
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height]  )
+    pyrosim.End()
 
 
-length, width, height = 1, 1, 1
-x,y,z = 0,0, height/2
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-           
-
-
-pyrosim.End()
+Create_World()
+Create_Robot()
