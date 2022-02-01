94a593bdcb3937ecc86f0de38b5d2a972e84331a
Lucia Carrera <luciaacarreraa@gmail.com>
2022-01-31 20:38:37 -0500

    Done: A two-link, one-joint robot.

--- generate.py
+++ generate.py
@@ -6,7 +6,8 @@ def Create_World():
     # cube is part of the world
     length, width, height = 1, 1, 1
     x,y, z = 0,5, height/2
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height]  )
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height]  )
+
     pyrosim.End()
 
 def Create_Robot():
@@ -16,9 +17,16 @@ def Create_Robot():
     length, width, height = 1, 1, 1
     x,y, z = 0,0, height/2
     pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height]  )
+
+    # joint between leg and torso (name should always be Parent_Child)
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5, 0, 1])
+    
+    # Leg
+    length, width, height = 1, 1, 1
+    x,y, z = 1.0,0,1.5
+    pyrosim.Send_Cube(name="Leg", pos=[x, y, z] , size=[length, width, height]  )
     pyrosim.End()
 
 
 Create_World()
 Create_Robot()
-print("here")
