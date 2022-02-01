5dfbce326f8fe1cdd6f19d49096be9f06440b81f
Lucia Carrera <luciaacarreraa@gmail.com>
2022-01-31 21:06:51 -0500

    hook done

--- generate.py
+++ generate.py
@@ -13,18 +13,38 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    # this cube is part of the robot
+    # this cube is part of the robot (Link 0) - uses abs coordinates
     length, width, height = 1, 1, 1
     x,y, z = 0,0, height/2
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height]  )
+    pyrosim.Send_Cube(name="Link0", pos=[x, y, z] , size=[length, width, height]  )
 
-    # joint between leg and torso (name should always be Parent_Child)
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5, 0, 1])
+    # joint between leg and torso (name should always be Parent_Child) - uses absolute coordinates
+    x, y, z = 0, 0, 1
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [x,y,z])
     
-    # Leg
+    # Leg (second link) Link 1# joint between leg and torso (name should always be Parent_Child) 
     length, width, height = 1, 1, 1
-    x,y, z = 1.0,0,1.5
-    pyrosim.Send_Cube(name="Leg", pos=[x, y, z] , size=[length, width, height]  )
+    x,y, z = 0,0,0.5
+    pyrosim.Send_Cube(name="Link1", pos=[x, y, z] , size=[length, width, height]  )
+    
+    # second joint - relative coordinates
+    x, y, z = 0,0,1
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [x, y, z])
+
+    # third link - relative coordinates
+    length, width, height = 1, 1, 1
+    x,y, z = 0,0,0.5
+    pyrosim.Send_Cube(name="Link2", pos=[x, y, z] , size=[length, width, height]  )
+
+    # third joint - relative coordinates
+    x, y, z = 0,0.5,0.5
+    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [x, y, z])
+
+    # fourth link - relative coordinates
+    length, width, height = 1, 1, 1
+    x,y, z = 0,0.5,0
+    pyrosim.Send_Cube(name="Link3", pos=[x, y, z] , size=[length, width, height]  )
+
     pyrosim.End()
 
 
