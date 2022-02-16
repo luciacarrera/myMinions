f311c94dfc6c1caa0666ff0678c4907e60e420c6
Lucia Carrera <luciaacarreraa@gmail.com>
2022-02-16 12:04:07 -0500

    created constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+import numpy
+PI = numpy.pi
+
+
+ITERATIONS = 1000
+GRAVITY = -50
+
+amplitudeFL = -PI/4
+amplitudeBL = +PI/4
+frequencyFL = 1/10
+frequencyBL = 1/20
+phaseOffsetFL = 5
+phaseOffsetBL = 0
+
+maxForce = 50
+
+startValue = -PI/4.0
+endValue =  PI/4.0
