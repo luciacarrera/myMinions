51fee3fdf572b715e3f6801c7e51b51f6825db73
Lucia Carrera <luciaacarreraa@gmail.com>
2022-02-28 11:39:24 -0500

    finished cleaning

--- motor.py
+++ motor.py
@@ -13,13 +13,13 @@ class MOTOR:
 
 
     def Set_Value(self, robotId, desiredAngle):
-        self.amplitude = c.amplitude
+        '''self.amplitude = c.amplitude
         self.offset = c.phaseOffset
         self.frequency = c.frequency # do we remove these???
 
         self.posJoint = self.amplitude * numpy.sin(self.frequency * desiredAngle + self.offset)
         values01 = numpy.linspace(c.startingValue, c.stoppingValue, c.ITERATIONS)
-        self.values =  numpy.sin(values01)
+        self.values =  numpy.sin(values01)'''
 
         pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = desiredAngle, maxForce = c.force)
    
